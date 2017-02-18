be3fcbf5f47ea4e5d53729696745e7edb020fc97
Agii Uri <Agii Uri>
2017-02-18 09:03:38 +0100

    Formating

--- scripts.js
+++ scripts.js
@@ -1,16 +1,16 @@
+
 $(document).ready(function() {
-var span = $('span');
-span.each(function(index, element) {
-	$('span:odd').css('color','red');
-});
+	var span = $('span');
+	span.each(function(index, element) {
+		$('span:odd').css('color','red');
+	});
 
-var paragraphs = $('p');
-paragraphs.each(function(index, element) {
-var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
-$(element).append(button) 
+	var paragraphs = $('p');
+	paragraphs.each(function(index, element) {
+		var button = '<button class="btn" data-tmp="' + index + '">Click me</button>';
+		$(element).append(button); 
+	});
+	$('button').click(function(){
+		alert($(this).attr('data-tmp'));
+	});
 });
-
-$('button').click(function(){
-	alert($(this).attr('data-tmp'));
-});
-})
